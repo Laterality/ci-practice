24d09016994012fde058cf86d45d51004dc987d3
Jinwoo Shin <bluesky0255@gmail.com>
2019-11-27 09:55:50 +0900

    Add base code

---
+++ src/main/java/kr/latera/cipractice/CiPracticeApplication.java
@@ -0,0 +1,13 @@
+package kr.latera.cipractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CiPracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/latera/cipractice/CiPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.latera.cipractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
