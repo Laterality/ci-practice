57042d3016b968c0c139fb7e582efef39ba9713c
Shin Jinwoo <bluesky0255@gmail.com>
2019-11-29 09:19:11 +0900

    Implement bye (#4)

--- src/main/java/kr/latera/cipractice/Greeter.java
+++ src/main/java/kr/latera/cipractice/Greeter.java
@@ -5,4 +5,8 @@ public class Greeter {
     public static String greet(String name) {
         return String.format("Hello, %s!", name);
     }
+
+    public static String bye(String name) {
+        return String.format("Bye, %s!", name);
+    }
 }
